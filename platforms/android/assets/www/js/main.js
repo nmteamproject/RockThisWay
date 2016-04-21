a4c379a63a113be57c7f2251039efad3bdcc792c
Eric Hunt <eth5881@rit.edu>
2016-04-21 09:04:51 -0400

    Styled survey more inline with the comps, added a results page (still needs to be editable)

--- platforms/android/assets/www/js/main.js
+++ platforms/android/assets/www/js/main.js
@@ -1,9 +1,10 @@
-import $ from './../libs/jquery/jquery'; 
-import { bootstrap } from './../libs/bootstrap/bootstrap.min.js';
+import './bootstrapHack'; 
+
 import { evothings } from './../libs/evothings/evothings';
 import { sortable } from './../libs/sortable/Sortable';
-import { attachFastClick } from 'fastclick';
+import './app';
+var attachFastClick = require('fastclick'); 
 
 // avoid the 300ms click delay on mobile devices
-attachFastClick(document.body);
+//attachFastClick(document.body);
 
