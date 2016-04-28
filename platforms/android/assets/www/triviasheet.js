1a8c2d782fa9b09f1d14a3ba8e0b018b8cdcc048
BKenny11 <bwk8958@rit.edu>
2016-04-28 09:38:37 -0400

    ATC Logic improved

--- platforms/android/assets/www/triviasheet.js
+++ platforms/android/assets/www/triviasheet.js
@@ -1,38 +1,28 @@
-var TriviaData = new Array(15);
-createTwoDimensionalArray(3);
+var TriviaData = new Array(10);
+createTwoDimensionalArray(2);
 
-TriviaData[0][0] = "Taylor Swift\'s song \"Shake It Off\" is from her album \"Red\".";
-TriviaData[1][0] = "I 100% know how all this code works.";
-TriviaData[2][0] = "The Rock and Roll hall of fame is in Cleveland Ohio.";
-TriviaData[3][0] = "Imagine RIT is on August 23rd.";
+TriviaData[0][0] = "The Beatles were not part of the 'British Invasion'.";
+TriviaData[1][0] = "The film 'Ferris Beullers day Off' featured the Bealtes' 'Hey Jude'.";
+TriviaData[2][0] = "Ringo Starr was the most popular Beatles' song writer.";
+TriviaData[3][0] = "The Beatles were known for their short hair.";
 TriviaData[4][0] = "Paul McCartney is a member of The Eagles.";
-TriviaData[5][0] = "This was coded by Brendan Kenny and a gay girl with blue hair.";
-TriviaData[6][0] = "This app was built using Cordova.";
-TriviaData[7][0] = "The Top Grossing Album of all time was made by Justin Beiber.";
-TriviaData[8][0] = "The lead singer of fun. is Nate Ruess.";
-TriviaData[9][0] = "The Who is a British Band";
-TriviaData[10][0] = "Rochester is a bipoler city.";
-TriviaData[11][0] = "Sharkrats are best rats.";
-TriviaData[12][0] = "Female charr have fluffy-er tails.";
-TriviaData[13][0] = "Japan has created a highly loved digital pop idol.";
-TriviaData[14][0] = "Akira Yamaoka composed the music for all Silent Hill games.";
+TriviaData[5][0] = "The Bealtes' first album was called 'My Bonnie'.";
+TriviaData[6][0] = "The Beatles broke up in 1970.";
+TriviaData[7][0] = "The Beatles' Bassist was named George Harrison.";
+TriviaData[8][0] = "'Love Me Do' was the Beatles' first hit.";
+TriviaData[9][0] = "George Martin is called the 5th Beatle";
 
 // Answers
-TriviaData[0][1] = "false";
-TriviaData[1][1] = "false";
-TriviaData[2][1] = "true";
-TriviaData[3][1] = "false";
-TriviaData[4][1] = "false";
-TriviaData[5][1] = "true";
-TriviaData[6][1] = "true";
-TriviaData[7][1] = "false";
-TriviaData[8][1] = "true";
-TriviaData[9][1] = "true";
-TriviaData[10][1] = "true";
-TriviaData[11][1] = "true";
-TriviaData[12][1] = "true";
-TriviaData[13][1] = "true";
-TriviaData[14][1] = "false";
+TriviaData[0][1] = "1";
+TriviaData[1][1] = "1";
+TriviaData[2][1] = "1";
+TriviaData[3][1] = "1";
+TriviaData[4][1] = "1";
+TriviaData[5][1] = "1";
+TriviaData[6][1] = "1";
+TriviaData[7][1] = "1";
+TriviaData[8][1] = "1";
+TriviaData[9][1] = "1";
 
 // Has question been asked
 // -- necessary because we are asking in random order
@@ -46,14 +36,53 @@ TriviaData[6][2] = "no";
 TriviaData[7][2] = "no";
 TriviaData[8][2] = "no";
 TriviaData[9][2] = "no";
-TriviaData[10][2] = "no";
-TriviaData[11][2] = "no";
-TriviaData[12][2] = "no";
-TriviaData[13][2] = "no";
-TriviaData[14][2] = "no";
 
+//Testing setup for possible multiple question type game.
+//Would be Answer 1
+TriviaData[0][3] = "1";
+TriviaData[1][3] = "1";
+TriviaData[2][3] = "1";
+TriviaData[3][3] = "1";
+TriviaData[4][3] = "1";
+TriviaData[5][3] = "1";
+TriviaData[6][3] = "1";
+TriviaData[7][3] = "1";
+TriviaData[8][3] = "1";
+TriviaData[9][3] = "1";
 
-
+//Answer 2
+TriviaData[0][4] = "2";
+TriviaData[1][4] = "2";
+TriviaData[2][4] = "2";
+TriviaData[3][4] = "2";
+TriviaData[4][4] = "2";
+TriviaData[5][4] = "2";
+TriviaData[6][4] = "2";
+TriviaData[7][4] = "2";
+TriviaData[8][4] = "2";
+TriviaData[9][4] = "2";
+//Answer 3
+TriviaData[0][5] = "3";
+TriviaData[1][5] = "3";
+TriviaData[2][5] = "3";
+TriviaData[3][5] = "3";
+TriviaData[4][5] = "3";
+TriviaData[5][5] = "3";
+TriviaData[6][5] = "3";
+TriviaData[7][5] = "3";
+TriviaData[8][5] = "3";
+TriviaData[9][5] = "3";
+//Answer 4
+TriviaData[0][6] = "4";
+TriviaData[1][6] = "4";
+TriviaData[2][6] = "4";
+TriviaData[3][6] = "4";
+TriviaData[4][6] = "4";
+TriviaData[5][6] = "4";
+TriviaData[6][6] = "4";
+TriviaData[7][6] = "4";
+TriviaData[8][6] = "4";
+TriviaData[9][6] = "4";
 
  function createTwoDimensionalArray(arraySize) {
             for (i = 0; i < TriviaData.length; ++i)
--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -3,19 +3,16 @@ module.exports = [
     {
         "file": "plugins/com.unarin.cordova.beacon/www/lib/underscore-min-1.6.js",
         "id": "com.unarin.cordova.beacon.underscorejs",
-        "pluginId": "com.unarin.cordova.beacon",
         "runs": true
     },
     {
         "file": "plugins/com.unarin.cordova.beacon/www/lib/q.min.js",
         "id": "com.unarin.cordova.beacon.Q",
-        "pluginId": "com.unarin.cordova.beacon",
         "runs": true
     },
     {
         "file": "plugins/com.unarin.cordova.beacon/www/LocationManager.js",
         "id": "com.unarin.cordova.beacon.LocationManager",
-        "pluginId": "com.unarin.cordova.beacon",
         "merges": [
             "cordova.plugins"
         ]
@@ -23,51 +20,86 @@ module.exports = [
     {
         "file": "plugins/com.unarin.cordova.beacon/www/Delegate.js",
         "id": "com.unarin.cordova.beacon.Delegate",
-        "pluginId": "com.unarin.cordova.beacon",
         "runs": true
     },
     {
         "file": "plugins/com.unarin.cordova.beacon/www/model/Region.js",
         "id": "com.unarin.cordova.beacon.Region",
-        "pluginId": "com.unarin.cordova.beacon",
         "runs": true
     },
     {
         "file": "plugins/com.unarin.cordova.beacon/www/Regions.js",
         "id": "com.unarin.cordova.beacon.Regions",
-        "pluginId": "com.unarin.cordova.beacon",
         "runs": true
     },
     {
         "file": "plugins/com.unarin.cordova.beacon/www/model/CircularRegion.js",
         "id": "com.unarin.cordova.beacon.CircularRegion",
-        "pluginId": "com.unarin.cordova.beacon",
         "runs": true
     },
     {
         "file": "plugins/com.unarin.cordova.beacon/www/model/BeaconRegion.js",
         "id": "com.unarin.cordova.beacon.BeaconRegion",
-        "pluginId": "com.unarin.cordova.beacon",
         "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
+        "id": "cordova-plugin-camera.Camera",
+        "clobbers": [
+            "Camera"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-camera/www/CameraPopoverOptions.js",
+        "id": "cordova-plugin-camera.CameraPopoverOptions",
+        "clobbers": [
+            "CameraPopoverOptions"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-camera/www/Camera.js",
+        "id": "cordova-plugin-camera.camera",
+        "clobbers": [
+            "navigator.camera"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-camera/www/CameraPopoverHandle.js",
+        "id": "cordova-plugin-camera.CameraPopoverHandle",
+        "clobbers": [
+            "CameraPopoverHandle"
+        ]
     },
     {
         "file": "plugins/cordova-plugin-device/www/device.js",
         "id": "cordova-plugin-device.device",
-        "pluginId": "cordova-plugin-device",
         "clobbers": [
             "device"
         ]
     },
     {
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-vibration/www/vibration.js",
+        "id": "cordova-plugin-vibration.notification",
+        "merges": [
+            "navigator.notification",
+            "navigator"
+        ]
+    },
+    {
         "file": "plugins/cordova-plugin-whitelist/whitelist.js",
         "id": "cordova-plugin-whitelist.whitelist",
-        "pluginId": "cordova-plugin-whitelist",
         "runs": true
     },
     {
         "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification.js",
         "id": "de.appplant.cordova.plugin.local-notification.LocalNotification",
-        "pluginId": "de.appplant.cordova.plugin.local-notification",
         "clobbers": [
             "cordova.plugins.notification.local",
             "plugin.notification.local"
@@ -76,7 +108,6 @@ module.exports = [
     {
         "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification-core.js",
         "id": "de.appplant.cordova.plugin.local-notification.LocalNotification.Core",
-        "pluginId": "de.appplant.cordova.plugin.local-notification",
         "clobbers": [
             "cordova.plugins.notification.local.core",
             "plugin.notification.local.core"
@@ -85,59 +116,9 @@ module.exports = [
     {
         "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification-util.js",
         "id": "de.appplant.cordova.plugin.local-notification.LocalNotification.Util",
-        "pluginId": "de.appplant.cordova.plugin.local-notification",
         "merges": [
             "cordova.plugins.notification.local.core",
             "plugin.notification.local.core"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
-        "id": "cordova-plugin-camera.Camera",
-        "pluginId": "cordova-plugin-camera",
-        "clobbers": [
-            "Camera"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-camera/www/CameraPopoverOptions.js",
-        "id": "cordova-plugin-camera.CameraPopoverOptions",
-        "pluginId": "cordova-plugin-camera",
-        "clobbers": [
-            "CameraPopoverOptions"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-camera/www/Camera.js",
-        "id": "cordova-plugin-camera.camera",
-        "pluginId": "cordova-plugin-camera",
-        "clobbers": [
-            "navigator.camera"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-camera/www/CameraPopoverHandle.js",
-        "id": "cordova-plugin-camera.CameraPopoverHandle",
-        "pluginId": "cordova-plugin-camera",
-        "clobbers": [
-            "CameraPopoverHandle"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
-        "id": "cordova-plugin-statusbar.statusbar",
-        "pluginId": "cordova-plugin-statusbar",
-        "clobbers": [
-            "window.StatusBar"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-vibration/www/vibration.js",
-        "id": "cordova-plugin-vibration.notification",
-        "pluginId": "cordova-plugin-vibration",
-        "merges": [
-            "navigator.notification",
-            "navigator"
         ]
     }
 ];
@@ -146,12 +127,12 @@ module.exports.metadata =
 {
     "com.unarin.cordova.beacon": "3.3.1",
     "cordova-plugin-app-event": "1.2.0",
-    "cordova-plugin-device": "1.1.1",
-    "cordova-plugin-whitelist": "1.2.1",
-    "de.appplant.cordova.plugin.local-notification": "0.8.4",
     "cordova-plugin-camera": "2.1.0",
+    "cordova-plugin-device": "1.1.1",
     "cordova-plugin-statusbar": "2.1.2",
-    "cordova-plugin-vibration": "2.1.0"
-}
+    "cordova-plugin-vibration": "2.1.0",
+    "cordova-plugin-whitelist": "1.2.1",
+    "de.appplant.cordova.plugin.local-notification": "0.8.4"
+};
 // BOTTOM OF METADATA
 });
